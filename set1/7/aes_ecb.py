6f83b293a31ef5912ed95c84926ef51e89a2ede6
Aditya Geria <adgeria@gmail.com>
2017-12-08 17:29:19 -0500

    7

---
+++ set1/7/aes_ecb.py
@@ -0,0 +1,9 @@
+from Crypto.Cipher import AES
+from base64 import b64decode
+
+with open("7.txt") as input_file:
+    cipher = b64decode(input_file.read())
+
+x = AES.new("YELLOW SUBMARINE", AES.MODE_ECB)
+plaintext = x.decrypt(cipher)
+print(plaintext)
